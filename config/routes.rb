bddbb831e14340ff8a6db26dfc0fca95a8aa65a0
栋栋晓 <2243980491@qq.com>
2016-03-18 00:17:56 +0800

    scaffold lists

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
+  resources :lists
   devise_for :views
   devise_for :users
+
+  root 'lists#index'
+
+
+
+
+
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160317160949) do
+ActiveRecord::Schema.define(version: 20160317161551) do
+
+  create_table "lists", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
